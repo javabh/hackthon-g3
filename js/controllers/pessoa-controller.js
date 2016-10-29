b907c02540fdce7fc5c09852000ad752d07fd704
Leonardo Lima <this.leotsl@gmail.com>
2016-10-29 12:51:59 -0200

    Criação de controllers de anuncio e pessoa, inclusão das chamadas dos serviços para consumo.

--- js/controllers/pessoa-controller.js
+++ js/controllers/pessoa-controller.js
@@ -5,9 +5,40 @@
 	.controller('PessoaCtrl', function ($scope, PessoaService) {
 
 
+		$scope.salvar = function(){
+			PessoaService.salvar($scope.pessoa).then(function(key){
+				if(key){
+					$scope.pessoa = {};
+					alert('Salvo com sucesso.');
+				}else{
+					alert('Erro.');
+				}
+			});
+		};
 
+		$scope.editar = function(){            		
+			PessoaService.editar($scope.pessoa).then(function(response){
+				if(key){
+					_getPessoa(key);
+					alert('Editado com sucesso.');
+				}else{
+					alert('Erro.');
+				}
+			})
+		};
 
+		function _getPessoa(key){
+			PessoaService.getPessoa(key).then(function(response){            			
+				$scope.pessoa = response.data;
+			});
+		}
 
+		function _getPessoas(){
+			PessoaService.getPessoas().then(function(response){
+				$scope.listaPessoas = response.data;
+			});
+		}
+			
 	});
 
 })(angular);
