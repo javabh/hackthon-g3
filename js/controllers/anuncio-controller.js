35be5ec4ff4d8e2b1e7e27f786d0348ca79f4f60
Maison Chaves <maison.chaves@gmail.com>
2016-10-29 13:48:55 -0200

    Anuncio

--- js/controllers/anuncio-controller.js
+++ js/controllers/anuncio-controller.js
@@ -1,49 +1,55 @@
 (function (ng) {
-    'use strict';
+      'use strict';
 
-    ng.module('controllers')
-    .controller('AnuncioCtrl', function ($scope,$state, $stateParams, AnuncioService) {				
+      ng.module('controllers')
+            .controller('AnuncioCtrl', function ($scope, $state, $stateParams, AnuncioService) {
 
-      $scope.salvar = function(){
-            AnuncioService.salvar($scope.anuncio).then(function(key){
-                  if(key){
-                        $scope.anuncio = {};
-                        alert('Salvo com sucesso.');
-                  }else{
-                        alert('Erro.');
+                  $scope.anuncio = {
+                        equipamento: {
+                              caracteristicas: []
+                        }
+                  };
+
+                  $scope.salvar = function () {
+                        AnuncioService.salvar($scope.anuncio).then(function (key) {
+                              if (key) {
+                                    $scope.anuncio = {};
+                                    alert('Salvo com sucesso.');
+                              } else {
+                                    alert('Erro.');
+                              }
+                        });
+                  };
+
+                  $scope.editar = function () {
+                        AnuncioService.editar($scope.anuncio).then(function (response) {
+                              if (key) {
+                                    _getAnuncio(key);
+                                    alert('Editado com sucesso.');
+                              } else {
+                                    alert('Erro.');
+                              }
+                        });
+                  };
+
+                  $scope.detalhes = function (key) {
+                        $state.transitionTo('detalhesList', {
+                              key: key
+                        });
+                        $scope.key = $state.key;
                   }
-            });
-      };
 
-      $scope.editar = function(){                     
-            AnuncioService.editar($scope.anuncio).then(function(response){
-                  if(key){
-                        _getAnuncio(key);
-                        alert('Editado com sucesso.');
-                  }else{
-                        alert('Erro.');
+                  function _getAnuncio(key) {
+                        AnuncioService._getAnuncio(key).then(function (response) {
+                              $scope.anuncio = response.data;
+                        });
                   }
+
+                  function _getAnuncios() {
+                        AnuncioService.getAnuncios().then(function (response) {
+                              $scope.listaAnuncios = response.data;
+                        });
+                  }
+
             });
-      };
-
-      $scope.detalhes = function(key){
-           $state.transitionTo('detalhesList', {
-              key: key                
-        });
-           $scope.key = $state.key;
-     }
-
-     function _getAnuncio(key){
-      AnuncioService._getAnuncio(key).then(function(response){                         
-            $scope.anuncio = response.data;
-      });
-}
-
-function _getAnuncios(){
-      AnuncioService.getAnuncios().then(function(response){
-            $scope.listaAnuncios = response.data;
-      });
-}
-
-});
 })(angular);
