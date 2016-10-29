4ee6c8ca51981b1de6383495435d02a6a6d8e1c1
Leonardo Lima <this.leotsl@gmail.com>
2016-10-29 13:59:27 -0200

    Tela de descrição

--- js/controllers/anuncio-controller.js
+++ js/controllers/anuncio-controller.js
@@ -1,49 +1,57 @@
-(function (ng) {
+(function(ng) {
     'use strict';
 
     ng.module('controllers')
-    .controller('AnuncioCtrl', function ($scope,$state, $stateParams, AnuncioService) {				
+        .controller('AnuncioCtrl', function($scope, $state, $stateParams, AnuncioService) {
 
-      $scope.salvar = function(){
-            AnuncioService.salvar($scope.anuncio).then(function(key){
-                  if(key){
+            $scope.salvar = function() {
+                AnuncioService.salvar($scope.anuncio).then(function(key) {
+                    if (key) {
                         $scope.anuncio = {};
                         alert('Salvo com sucesso.');
-                  }else{
+                    } else {
                         alert('Erro.');
-                  }
-            });
-      };
+                    }
+                });
+            };
 
-      $scope.editar = function(){                     
-            AnuncioService.editar($scope.anuncio).then(function(response){
-                  if(key){
+            $scope.editar = function() {
+                AnuncioService.editar($scope.anuncio).then(function(response) {
+                    if (key) {
                         _getAnuncio(key);
                         alert('Editado com sucesso.');
-                  }else{
+                    } else {
                         alert('Erro.');
-                  }
+                    }
+                });
+            };
+
+            $scope.detalhes = function(key) {
+                $state.transitionTo('detalhesList', {
+                    key: key
+                });
+
+            };
+
+            function _getAnuncio(key) {
+                AnuncioService.getAnuncio(key).then(function(response) {
+                    $scope.anuncio = response.data;
+                });
+            }
+
+            function _getAnuncios() {
+                AnuncioService.getAnuncios().then(function(response) {
+                    $scope.listaAnuncios = response.data;
+                });
+            }
+
+            $scope.$on('$viewContentLoaded', function() {
+                if ($stateParams.key) {
+                    _getAnuncio($stateParams.key);
+                } else {
+                    _getAnuncios();
+                }
             });
-      };
 
-      $scope.detalhes = function(key){
-           $state.transitionTo('detalhesList', {
-              key: key                
         });
-           $scope.key = $state.key;
-     }
-
-     function _getAnuncio(key){
-      AnuncioService._getAnuncio(key).then(function(response){                         
-            $scope.anuncio = response.data;
-      });
-}
-
-function _getAnuncios(){
-      AnuncioService.getAnuncios().then(function(response){
-            $scope.listaAnuncios = response.data;
-      });
-}
-
-});
 })(angular);
